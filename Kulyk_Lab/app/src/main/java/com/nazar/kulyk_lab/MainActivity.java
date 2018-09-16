475bb3e08fb66768154ba7610b0edb5d99a9b867
DESKTOP-QUABODL\nazar <nazar.kulik99@gmail.com>
2018-09-17 00:12:58 +0300

    Finish lab3

--- Kulyk_Lab/app/src/main/java/com/nazar/kulyk_lab/MainActivity.java
+++ Kulyk_Lab/app/src/main/java/com/nazar/kulyk_lab/MainActivity.java
@@ -1,7 +1,6 @@
 package com.nazar.kulyk_lab;
 
 import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,13 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import javax.xml.validation.Validator;
+
 public class MainActivity extends AppCompatActivity {
     protected TextView result;
+    protected String text;
     protected EditText first_name;
     protected EditText last_name;
+    protected EditText email;
+    protected EditText phone;
     protected EditText password;
     protected EditText confirm_password;
     protected Button submit_button;
+    protected Boolean validatorResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +29,15 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
         first_name = findViewById(R.id.first_name);
         last_name = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+        phone = findViewById(R.id.phone);
         password = findViewById(R.id.password);
         confirm_password = findViewById(R.id.confirm_password);
         submit_button = findViewById(R.id.submit_button);
         onClickButtonsHandler();
     }
 
-    public void onClickButtonsHandler(){
+    public void onClickButtonsHandler() {
 
         submit_button.setOnClickListener(new View.OnClickListener() {
 
@@ -38,28 +45,56 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             public void onClick(View v) {
                 result.setText("");
-                String first_name_value = String.valueOf(first_name.getText());
-                NameValidator(first_name_value, "first name", first_name);
-                String last_name_value = String.valueOf(last_name.getText());
-                NameValidator(last_name_value, "last name", last_name);
+                validatorResult = true;
+                StringValidator(first_name, "^[A-Z][a-zA-Z]+$", "first name");
+                StringValidator(last_name, "^[A-Z][a-zA-Z]+$", "last name");
+                StringValidator(email, "^[a-zA-Z0-9+_.-]+@[a-zA-Z]+\\.[A-Za-z]{2,4}$",
+                        "email");
+                StringValidator(phone, "^\\+?[0-9]{10,16}$", "phone");
+                StringValidator(password,
+                        "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+                        "password");
+                PasswordsCheck();
+                if(validatorResult){
+                    result.setText("All fields are ok");
+                }
             }
         });
 
     }
 
-    public void NameValidator(String value, String field, EditText field_id){
+    @SuppressLint("SetTextI18n")
+    public void StringValidator(EditText field_id, String regex, String field_name){
+        field_id.setError(null);
+        String value = String.valueOf(field_id.getText());
+        String already_in_result = String.valueOf(result.getText());
         if(value.equals("")){
-            field_id.setHighlightColor(Color.RED);
-            String text = String.valueOf(result.getText()) + "\nEmpty " + field;
-            result.setText(text);
-        }
-        else if(!(value.matches("[A-Z][a-zA-Z]+"))){
-            field_id.setHighlightColor(Color.RED);
-            String text = String.valueOf(result.getText()) + "\nIncorrect " + field;
-            result.setText(text);
+            validatorResult = false;
+            result.setText(already_in_result + "\nEmpty " + field_name);
+            field_id.setError("Empty " + field_name);
+        } else if (!(value.matches(regex))){
+            validatorResult = false;
+            result.setText(already_in_result + "\nIncorrect " + field_name);
+            field_id.setError("Incorrect " + field_name);
         }
     }
 
-    public void PasswordValidator(String password, String confirm_password){
+    @SuppressLint("SetTextI18n")
+    public void PasswordsCheck(){
+        confirm_password.setError(null);
+        String password_value = String.valueOf(password.getText());
+        String confirm_password_value = String.valueOf(confirm_password.getText());
+        if(confirm_password_value.equals("") && (password_value.equals(""))){
+            validatorResult = false;
+            confirm_password.setError("Empty confirm password");
+            String already_in_result = String.valueOf(result.getText());
+            result.setText(already_in_result + "\nEmpty confirm password");
+        }
+        if(!(password_value.equals(confirm_password_value))){
+            validatorResult = false;
+            confirm_password.setError("Password don`t match");
+            String already_in_result = String.valueOf(result.getText());
+            result.setText(already_in_result + "\nPasswords don`t match");
+        }
     }
 }
